c0eb0cb482a755275087c52751f374404f8e1ed4
HyeonuPark <nemo1275@gmail.com>
2015-10-04 05:09:58 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var insert = require('gulp-insert');
+
+var replaceRegExp = /(\s)(let|var|const ) *sibling *\;/;
+
+module.exports = function(myself, sibling) {
+  return insert.transform(function(contents, file) {
+    return contents.replace(replaceRegExp, function(match, headSpace, type) {
+      return headSpace+
+        type+'siblingPathSep = require("path").sep;'+
+        type+'sibling = require(__dirname.split(siblingPathSep).reverse()'+
+          '.map(function(dir) {'+
+            'if (this.found && dir == '+myself+') {'+
+              'return dir;'+
+            '} else {'+
+              'this.found = true;'+
+              'return '+sibling+';'+
+            '}'+
+          '}, {})'+
+          '.reverse().join(siblingPathSep) + __filename);';
+    });
+  });
+};
