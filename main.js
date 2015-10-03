494e8238b4e2f22774e72f103c88b7cd4c15959a
HyeonuPark <nemo1275@gmail.com>
2015-10-04 06:38:46 +0900

    check eslint

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 var insert = require('gulp-insert');
 
-var replaceRegExp = /(\s|^)(let |var |const ) *sibling.*?\;/;
+var replaceRegExp = /(\s|^)(let |var |const ) *sibling.*?\; ?/;
 
 module.exports = function(myself, sibling) {
   return insert.transform(function(contents) {
     return contents.replace(replaceRegExp, function(match, headSpace, type) {
       return headSpace+
-        type+'siblingPathSep = require("path").sep;'+
+        type+'siblingPathSep = require(\'path\').sep; '+
         type+'sibling = require(__filename.split(siblingPathSep).reverse()'+
           '.map(function(dir) {'+
-            'if (!this.found && dir == "'+myself+'") {'+
-              'this.found = true;'+
-              'return "'+sibling+'";'+
+            'if (!this.found && dir === \''+myself+'\') {'+
+              'this.found = true; '+
+              'return \''+sibling+'\'; '+
             '} else {'+
-              'return dir;'+
+              'return dir; '+
             '}'+
           '}, {})'+
-          '.reverse().join(siblingPathSep));';
+          '.reverse().join(siblingPathSep)); ';
     });
   });
 };
